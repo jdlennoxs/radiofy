fea26e2a0da086966f491c27de8bdf87f08b64be
Jack Scott <jack.scott10@gmail.com>
2022-08-14 16:37:40 +0100

    Adds spotify api calls

--- src/server/router/spotify.ts
+++ src/server/router/spotify.ts
@@ -7,9 +7,28 @@ export const spotifyRouter = createRouter()
       return await ctx.spotify.getMyDevices();
     },
   })
+  .query("getUserPlaylists", {
+    async resolve({ ctx }) {
+      return await ctx.spotify.getUserPlaylists();
+    },
+  })
+  .query("getPlaylist", {
+    input: z.object({
+      id: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.spotify.getPlaylist(input.id);
+    },
+  })
   .query("getCurrentTrack", {
     async resolve({ ctx }) {
       // spotifyApi.setAccessToken(ctx.session.accessToken);
       return await ctx.spotify.getMyCurrentPlayingTrack();
+    },
+  })
+  .mutation("startPlayback", {
+    input: z.string().array(),
+    async resolve({ ctx, input }) {
+      return await ctx.spotify.play({ uris: input });
     },
   });
